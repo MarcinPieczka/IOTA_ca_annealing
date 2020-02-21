5fec54e735470f9b610f423b1c3e8a2e9e6a6730
Marcin Pieczka <marcin.pieczka@gmail.com>
2020-02-21 23:06:50 +0100

    working version

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,73 @@
 use rand::prelude::*;
-use std::iter;
+use std::time::Instant;
 
 struct Node {
     vote: bool,
-    neighbours: Vec<*const bool>
+    neighbours: Vec<i16>
 }
 
 fn main() {
     // generate nodes
-    let mut nodes = Vec::new();
-    for _ in 0..1000 {
-        nodes.push(Node{
-            vote: random(),
-            neighbours: Vec::new()
-        })
-    }
-    // generate connections
-    for i in 0..1000 {
-        let mut excluded = vec![i];
-        for _ in 0..4 {
-            let mut x;
-            loop {
-                x = thread_rng().gen_range(0, 1000);
-                if !excluded.contains(&x) {
-                    excluded.push(x);
-                    break;
+    let start = Instant::now();
+    let mut total = 0;
+    let mut failed = 0;
+    loop {
+        total += 1;
+        let mut nodes = Vec::new();
+        for _ in 0..1000 {
+            nodes.push(Node{
+                vote: random(),
+                neighbours: Vec::new()
+            })
+        }
+        // generate connections
+        for i in 0..1000 {
+            let mut excluded = vec![i];
+            for _ in 0..4 {
+                let mut x;
+                loop {
+                    x = thread_rng().gen_range(0, 1000);
+                    if !excluded.contains(&x) {
+                        excluded.push(x);
+                        break;
+                    }
                 }
+                nodes[i].neighbours.push(x as i16);
+                nodes[x].neighbours.push(i as i16);
             }
-            let mut ptr = nodes[x].vote;
-            nodes[i].neighbours.push(&ptr);
-            ptr = nodes[i].vote;
-            nodes[x].neighbours.push(&ptr);
+        }
+        let mut i = 0;
+        loop {
+            i += 1;
+            let res = vote_proportions(&nodes);
+            one_epoch(&mut nodes);
+            if i > 1000 {
+                failed += 1;
+                println!(
+                    "fail ratio: {}%, {} 1/s",
+                    100.0 * failed as f32 / total as f32,
+                    total as f32 / start.elapsed().as_secs_f32()
+                )
+            }
+            if res == 1.0 || res == 0.0 || i > 1000 {
+                break;
+            }
         }
     }
-    println!("{:?}", nodes[0].neighbours);
-    // run simulation
-    println!("{}", vote_proportions(&nodes));
-    one_epoch(&mut nodes);
-    println!("{}", vote_proportions(&nodes));
-    one_epoch(&mut nodes);
-    println!("res: {}", vote_proportions(&nodes));
+
 
 }
 
 fn one_epoch(nodes: &mut Vec<Node>){
-    for mut node in nodes{
-        unsafe {
-            let n_of_t = node.neighbours.iter().map(|&x| *x)
-                .chain(iter::once(node.vote))
-                .filter(|&x| x)
-                .count() as f32;
-            let all = node.neighbours.len() as f32 / 2.0;
-            println!("{:?}", node.neighbours.iter().map(|&x| *x));
-            println!("{} {}", n_of_t, all);
-            node.vote = n_of_t > all;
+    for i in 0..nodes.len(){
+        let n_true = nodes[i].neighbours.iter().map(|&x| nodes[x as usize].vote)
+            .filter(|&x| x)
+            .count();
+        let all = nodes[i].neighbours.len();
+        if n_true * 2 == all {
+            nodes[i].vote = !nodes[i].vote;
+        } else {
+            nodes[i].vote = (all + 2) / (n_true + 1) == 1;
         }
     }
 }
