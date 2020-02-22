1cf606bc4a1fb77b76e301131b0b8412f397ea4a
Marcin Pieczka <marcin.pieczka@gmail.com>
2020-02-22 15:41:30 +0100

    refactored

--- src/main.rs
+++ src/main.rs
@@ -11,52 +11,71 @@ fn main() {
     let start = Instant::now();
     let mut total = 0;
     let mut failed = 0;
+
     loop {
         total += 1;
-        let n = thread_rng().gen_range(1000, 10000);
-        let mut nodes = Vec::new();
-        for _ in 0..n {
-            nodes.push(Node{
-                vote: random(),
-                neighbours: Vec::new()
-            })
-        }
-        // generate connections
-        for i in 0..n {
-            let mut excluded = vec![i];
-            for _ in 0..4 {
-                let mut x;
-                loop {
-                    x = thread_rng().gen_range(0, n);
-                    if !excluded.contains(&x) {
-                        excluded.push(x);
-                        break;
-                    }
-                }
-                nodes[i].neighbours.push(x as i16);
-                nodes[x].neighbours.push(i as i16);
-            }
-        }
-        let mut i = 0;
-        loop {
-            i += 1;
-            let res = vote_proportions(&nodes);
-            one_epoch(&mut nodes);
-            if i > 1000 {
-                failed += 1;
-                println!(
-                    "fail ratio: {}%, {} 1/s",
-                    100.0 * failed as f32 / total as f32,
-                    total as f32 / start.elapsed().as_secs_f32()
-                )
-            }
-            if res == 1.0 || res == 0.0 || i > 1000 {
-                break;
-            }
+        if !one_voting() {
+            failed += 1;
+            println!(
+                "fail ratio: {}%, {} 1/s",
+                100.0 * failed as f32 / total as f32,
+                total as f32 / start.elapsed().as_secs_f32()
+            )
         }
     }
+}
 
+fn one_voting() -> bool {
+    let result: bool;
+    let mut nodes = gen_arrow_greph();
+    let mut i = 0;
+    loop {
+        i += 1;
+        let res = vote_proportions(&nodes);
+        one_epoch(&mut nodes);
+        if i > 1000 {
+            result = false;
+            break
+        }
+        if res == 1.0 || res == 0.0 {
+            result = true;
+            break;
+        }
+    }
+    result
+}
 
+fn gen_arrow_greph() -> Vec<Node>{
+    let n = thread_rng().gen_range(1000, 10000);
+    let mut nodes = initialize_graph(n);
+    // generate connections
+    for i in 0..n {
+        let mut excluded = vec![i];
+        for _ in 0..4 {
+            let mut x;
+            loop {
+                x = thread_rng().gen_range(0, n);
+                if !excluded.contains(&x) {
+                    excluded.push(x);
+                    break;
+                }
+            }
+            nodes[i].neighbours.push(x as i16);
+            nodes[x].neighbours.push(i as i16);
+        }
+    }
+    nodes
+}
+
+fn initialize_graph(n: usize) -> Vec<Node>{
+    let mut nodes = Vec::new();
+    for _ in 0..n {
+        nodes.push(Node{
+            vote: random(),
+            neighbours: Vec::new()
+        })
+    }
+    nodes
 }
 
 fn one_epoch(nodes: &mut Vec<Node>){
